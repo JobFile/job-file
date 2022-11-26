7db91d0e9ce5b3d542321f623611ce9bfecd7bda
Firas K <firaskhansa3@gmail.com>
2022-11-26 12:01:40 -0500

    Added routes for updating database

--- server/server.js
+++ server/server.js
@@ -2,12 +2,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = 3000
-
-const PG_URI = ''
+const jobRouter = require('./routes/route.js')
 
 // TODO: connect pg DB
 
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../src/index.html'))
+})
+
+app.post('/login', (req, res) => {
+  res.status(201).send('login reached'/* add cookie here? */)
+})
+
+app.use('/jobApp', jobRouter)
 
 app.use('*', (req, res) => {
   res.sendStatus(404)
@@ -21,10 +30,6 @@ app.use((err, req, res, next) => {
   }
   const errorObj = Object.assign(defaultErr, err)
   return JSON.stringify(res.status(errorObj.status).send(errorObj.message))
-})
-
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../src/index.html'))
 })
 
 app.listen(PORT, () => {
