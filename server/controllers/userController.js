765cb2c3f0ac0b61473377226310af703f82d48e
Firas K <firaskhansa3@gmail.com>
2022-11-26 18:07:15 -0500

    added encryption via bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,17 @@
 const db = require('../models')
+const bcrypt = require('bcryptjs');
 
 const userController = {}
 
-userController.createUser = (req, res, next) => {
+userController.createUser = async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body
   const newUser = `INSERT INTO Users (first_name, last_name, email, password)
   VALUES ($1, $2, $3, $4) RETURNING *`
-  const values = [firstName, lastName, email, password]
-
+  const hash = await bcrypt.hash(password, 10);
+  const values = [firstName, lastName, email, hash];
   db.query(newUser, values)
     .then(data => {
-      // console.log(data.rows[0])
+      console.log(data.rows[0])
       if (data.rows[0] === undefined) {
         return next({
           log: 'createUser',
@@ -20,7 +21,8 @@ userController.createUser = (req, res, next) => {
       res.locals.application = data.rows[0]
       console.log(res.locals.application)
       return next()
-    }).catch(() => {next({
+    }).catch(() => {
+      return next({
       log: 'userController.createUser',
       message: { err: 'error inside create user controller' }
     });
@@ -32,20 +34,24 @@ userController.createUser = (req, res, next) => {
 userController.verifyUser = (req, res, next) => {
   const { email, password } = req.body
   const verifyQuery = `SELECT u.email, u.password, u.user_id from Users u
-  WHERE u.email = $1 AND u.password = $2`
-  const values = [email, password]
+  WHERE u.email = $1`
+  const values = [email]
 
   db.query(verifyQuery, values)
     .then(data => {
       console.log(data.rows[0])
-      if (data.rows[0] === undefined) {
+      console.log(password)
+      const bool = bcrypt.compareSync(password, data.rows[0].password);
+      if (bool) {
+        console.log('PASSWORDS MATCH')
+        res.locals.userID = data.rows[0].user_id;
+        return next();
+      } else {
         return next({
           log: 'verifyUser',
           message: { err: 'ERROR: Enter valid user' }
-        })
+        });
       }
-      res.locals.userID = data.rows[0].user_id.toString();
-      next();
     }).catch(() => { next({
       log: 'userController.verifyUser',
       message: { err: 'error inside create user controller' }
--- server/models.js
+++ server/models.js
@@ -3,6 +3,9 @@ const { Pool } = require('pg');
 // TODO: require db
 require('dotenv').config();
 
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+
 const pool = new Pool({
   connectionString: process.env.PG_URI
 });
