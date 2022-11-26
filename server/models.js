43d5a212cfdd2e4e30af826a7b840a5df8b89e46
BrynnSakell <brynnsakell@gmail.com>
2022-11-26 12:06:27 -0500

    set up instance on elephant

--- server/models.js
+++ server/models.js
@@ -1,3 +1,18 @@
 
 const { Pool } = require('pg')
 // TODO: require db
+const PG_URI = 'postgres://tsnzxkew:8jsRfwoz7gVYHiirE96YrCXjLb7lf3cP@otto.db.elephantsql.com/tsnzxkew';
+
+const pool = new Pool({
+  connectionString: PG_URI
+});
+
+
+
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  }
+};
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
-const PG_URI = ''
+// const PG_URI = ''
 
 // TODO: connect pg DB
 
